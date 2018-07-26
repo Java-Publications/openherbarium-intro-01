f5fa28aa8bf2b8104ea32a7e97f7674ff4657e1a
dve <dve@vergien.net>
2018-07-26 13:09:14 +0200

    A dummy detail view

---
+++ src/main/java/org/rapidpm/openherbarium/module/ui/component/mainview/detailview/DetailView.java
@@ -0,0 +1,22 @@
+package org.rapidpm.openherbarium.module.ui.component.mainview.detailview;
+
+import java.net.MalformedURLException;
+import javax.annotation.PostConstruct;
+import org.infinitenature.leafletzoomify.LZoomifyImage;
+import org.rapidpm.dependencies.core.logger.HasLogger;
+import com.vaadin.ui.Composite;
+import com.vaadin.ui.Label;
+
+public class DetailView extends Composite implements HasLogger {
+  @PostConstruct
+  private void postConstruct() {
+    try {
+      final LZoomifyImage zoomifyImage =
+          new LZoomifyImage("https://images.gfw-online.org/47267/", "&copy; GFW-Online");
+      setCompositionRoot(zoomifyImage);
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+      setCompositionRoot(new Label("Hello Open Herbarium"));
+    }
+  }
+}
